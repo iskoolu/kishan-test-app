12c297a5298e73883e05e0270eac8f5831089e5d
Jola Arun <arun21101998@gmail.com>
2023-04-05 13:06:33 +0530

    changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,37 @@
-import BurgerMenu from "../../styled-component/Burgermenu"
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import Image from "next/image"
 import React from "react"
 import logo from "../../public/logo.png"
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 import { useRouter } from "next/router"
+import Link from "next/link";
 
 function Navbar() {
       const router = useRouter()
+      const [dropdownOpen, setDropdownOpen] = useState(false);
+
+      const handleDropdownOpen = () => {
+            setDropdownOpen(true);
+      };
+
+      const dropdownRef = useRef(null);
+
+      const handleOutsideClick = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                  setDropdownOpen(false);
+            }
+      };
+
+      useEffect(() => {
+            document.addEventListener('click', handleOutsideClick, true);
+            return () => {
+                  document.removeEventListener('click', handleOutsideClick, true);
+            };
+      }, []);
+
+
 
       return (
             <header className="fixed w-[100%] top-0 z-50 bg-lightblack flex items-center justify-between h-50 pr-3 pl-1 py-1 lg:px-7 lg:py-1">
@@ -18,13 +44,45 @@ function Navbar() {
                               {router.pathname === "/Menu" ? "Menu" : "We're on a mission"}
                         </p>
                   </div>
-                  <div className="hidden lg:block ">
-                        <BurgerMenu screenType="desktop" />
+
+                  <div className="relative" ref={dropdownRef}>
+                        <MenuOpenIcon
+                              className="flex items-center text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 mt-2 " sx={{ color: 'white' }}
+                              onClick={handleDropdownOpen}
+                        >
+                              <span>Dropdown</span>
+                              <svg
+                                    className="w-4 h-4 ml-2"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    stroke="currentColor"
+                              >
+                                    <path
+                                          d="M19 9l-7 7-7-7"
+                                          strokeLinecap="round"
+                                          strokeLinejoin="round"
+                                          strokeWidth="2"
+                                    />
+                              </svg>
+                        </MenuOpenIcon>
+                        {dropdownOpen && (
+                              <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-lg z-10 bg-textwhite">
+                                    <Link href="/" legacyBehavior>
+                                          <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 text-lightblack">Services</a>
+                                    </Link>
+                                    <Link href="/Environment" legacyBehavior>
+                                          <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 text-lightblack">Environment Statement</a>
+                                    </Link>
+                                    <Link href="/Faq" legacyBehavior>
+                                          <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 text-lightblack">FAQ</a>
+                                    </Link>
+                              </div>
+                        )
+                        }
                   </div>
-                  <div className="lg:hidden block mr-[8px]">
-                        <BurgerMenu screenType="mobile" />
-                  </div>
-            </header>
+
+            </header >
       )
 }
 
--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,32 +1,138 @@
-import { faqContent } from "../../staticdata/appContents"
-import Accordion from "../../styled-component/Accordion"
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
 import React, { useState } from "react"
 
 function Faq() {
-      const [expanded, setExpanded] = useState("1")
-      const handleChange = val => {
-            if (val !== expanded) {
-                  setExpanded(val)
-            } else {
-                  setExpanded("0")
-            }
-      }
+
+      const [expanded, setExpanded] = React.useState(false);
+      const handleAccordionChange = (panel) => (event, newExpanded) => {
+            setExpanded(newExpanded ? panel : '');
+
+      };
+
       return (
             <div>
                   <h1 className="text-[45px] lg:text-9xl mb-[3rem] font-bold uppercase">
                         Frequently Asked
                   </h1>
-                  {faqContent.map((faqQues, id) => (
-                        <div className="mt-6" key={id}>
-                              <Accordion
-                                    questionId={faqQues.questionId}
-                                    title={faqQues.question}
-                                    content={faqQues.answer}
-                                    expanded={expanded}
-                                    handleChange={handleChange}
-                              />
-                        </div>
-                  ))}
+
+                  <Accordion className="w-[100%] bg-lightblack mt-6 lg:px-7 lg:py-1  pr-3 pl-1 py-1 " sx={{ backgroundColor: 'black' }} expanded={expanded === 'panel1'} onChange={handleAccordionChange('panel1')} TransitionProps={{ unmountOnExit: true }} >
+                        <AccordionSummary expandIcon={
+                              expanded === 'panel1' ? (
+                                    <AddIcon sx={{ color: 'whitesmoke' }} className="flex items-end" />
+                              ) : (
+                                    <RemoveIcon sx={{ color: 'whitesmoke' }} className="flex items-end ml-[10px]" />
+                              )
+                        }
+                              aria-controls="accordian-content"
+                              id="panel1a-header">
+                              <Typography className="font-bold  whitespace-nowrap text-textwhite text-[22px] " >What is Node Army?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                              <Typography className="text-textwhite text-[19px]">
+                                    Node Army is a cloud-native blockchain infrastructure provider that empowers individuals and businesses to deploy and manage nodes, even without technical expertise. Our infrastructure is secure and scalable, with advanced features such as co-nodes, compounding, and managed nodes.
+                              </Typography>
+                        </AccordionDetails>
+                  </Accordion>
+                  <Accordion className="w-[100%] bg-lightblack mt-6 lg:px-7 lg:py-1  pr-3 pl-1 py-1 " sx={{ backgroundColor: 'black' }} expanded={expanded === 'panel2'} onChange={handleAccordionChange('panel2')} TransitionProps={{ unmountOnExit: true }}>
+                        <AccordionSummary expandIcon={
+                              expanded === 'panel2' ? (
+                                    <AddIcon sx={{ color: 'whitesmoke' }} />
+                              ) : (
+                                    <RemoveIcon sx={{ color: 'whitesmoke' }} />
+                              )
+                        }
+                              aria-controls="accordian-content"
+                              id="panel1a-header">
+                              <Typography className="font-bold  whitespace-nowrap text-textwhite text-[12px] flex-shrink-0 md:text-[8px]" >How can I benefit from using Node Army?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                              <Typography className="text-textwhite text-[19px]">
+                                    Node Army is a cloud-native blockchain infrastructure provider that empowers individuals and businesses to deploy and manage nodes, even without technical expertise. Our infrastructure is secure and scalable, with advanced features such as co-nodes, compounding, and managed nodes.
+                              </Typography>
+                        </AccordionDetails>
+                  </Accordion>
+
+                  <Accordion className="w-[100%] bg-lightblack mt-6 lg:w-full lg:px-7 lg:py-1 sm:w-full md:w-full pr-3 pl-1 py-1 " sx={{ backgroundColor: 'black' }} expanded={expanded === 'panel3'} onChange={handleAccordionChange('panel3')} TransitionProps={{ unmountOnExit: true }}>
+                        <AccordionSummary expandIcon={
+                              expanded === 'panel3' ? (
+                                    <AddIcon sx={{ color: 'whitesmoke' }} />
+                              ) : (
+                                    <RemoveIcon sx={{ color: 'whitesmoke' }} />
+                              )
+                        }
+                              aria-controls="accordian-content"
+                              id="panel1a-header">
+                              <Typography className="font-bold  whitespace-nowrap text-textwhite text-[22px] " >Do I need technical expertise to use Node Army?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                              <Typography className="text-textwhite text-[19px]">
+                                    Node Army is a cloud-native blockchain infrastructure provider that empowers individuals and businesses to deploy and manage nodes, even without technical expertise. Our infrastructure is secure and scalable, with advanced features such as co-nodes, compounding, and managed nodes.
+                              </Typography>
+                        </AccordionDetails>
+                  </Accordion>
+
+                  <Accordion className="w-[100%] bg-lightblack mt-6 lg:w-full lg:px-7 lg:py-1 sm:w-full md:w-full pr-3 pl-1 py-1 " sx={{ backgroundColor: 'black' }} expanded={expanded === 'panel4'} onChange={handleAccordionChange('panel4')} TransitionProps={{ unmountOnExit: true }}>
+                        <AccordionSummary expandIcon={
+                              expanded === 'panel4' ? (
+                                    <AddIcon sx={{ color: 'whitesmoke' }} />
+                              ) : (
+                                    <RemoveIcon sx={{ color: 'whitesmoke' }} />
+                              )
+                        }
+                              aria-controls="accordian-content"
+                              id="panel1a-header">
+                              <Typography className="font-bold  whitespace-nowrap text-textwhite text-[22px] " >What kind of support does Node Army provide?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                              <Typography className="text-textwhite text-[19px]">
+                                    Node Army is a cloud-native blockchain infrastructure provider that empowers individuals and businesses to deploy and manage nodes, even without technical expertise. Our infrastructure is secure and scalable, with advanced features such as co-nodes, compounding, and managed nodes.
+                              </Typography>
+                        </AccordionDetails>
+                  </Accordion>
+
+                  <Accordion className="w-[100%] bg-lightblack mt-6 lg:w-full lg:px-7 lg:py-1 sm:w-full md:w-full pr-3 pl-1 py-1 " sx={{ backgroundColor: 'black' }} expanded={expanded === 'panel5'} onChange={handleAccordionChange('panel5')} TransitionProps={{ unmountOnExit: true }}>
+                        <AccordionSummary expandIcon={
+                              expanded === 'panel5' ? (
+                                    <AddIcon sx={{ color: 'whitesmoke' }} />
+                              ) : (
+                                    <RemoveIcon sx={{ color: 'whitesmoke' }} />
+                              )
+                        }
+                              aria-controls="accordian-content"
+                              id="panel1a-header">
+                              <Typography className="font-bold  whitespace-nowrap text-textwhite text-[22px] " >How do I get started with Node Army?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                              <Typography className="text-textwhite text-[19px]">
+                                    Node Army is a cloud-native blockchain infrastructure provider that empowers individuals and businesses to deploy and manage nodes, even without technical expertise. Our infrastructure is secure and scalable, with advanced features such as co-nodes, compounding, and managed nodes.
+                              </Typography>
+                        </AccordionDetails>
+                  </Accordion>
+
+                  <Accordion className="w-[100%] bg-lightblack mt-6 lg:w-full lg:px-7 lg:py-1 sm:w-full md:w-full pr-3 pl-1 py-1 " sx={{ backgroundColor: 'black' }} expanded={expanded === 'panel6'} onChange={handleAccordionChange('panel6')} TransitionProps={{ unmountOnExit: true }}>
+                        <AccordionSummary expandIcon={
+                              expanded === 'panel6' ? (
+                                    <AddIcon sx={{ color: 'whitesmoke' }} />
+                              ) : (
+                                    <RemoveIcon sx={{ color: 'whitesmoke' }} />
+                              )
+                        }
+                              aria-controls="accordian-content"
+                              id="panel1a-header">
+                              <Typography className="font-bold  whitespace-nowrap text-textwhite text-[22px] " >How will I be rewarded by using Node Army?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                              <Typography className="text-textwhite text-[19px]">
+                                    Node Army is a cloud-native blockchain infrastructure provider that empowers individuals and businesses to deploy and manage nodes, even without technical expertise. Our infrastructure is secure and scalable, with advanced features such as co-nodes, compounding, and managed nodes.
+                              </Typography>
+                        </AccordionDetails>
+                  </Accordion>
+
             </div>
       )
 }
